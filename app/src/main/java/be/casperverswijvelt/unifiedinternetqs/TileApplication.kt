a08b3831907c2d349768be5f00ef7c1dc758dbc3
D3SOX <nico@d3sox.me>
2024-03-18 22:46:19 +0100

    feat: remove firebase, analytics and all build flavors

--- app/src/main/java/be/casperverswijvelt/unifiedinternetqs/TileApplication.kt
+++ app/src/main/java/be/casperverswijvelt/unifiedinternetqs/TileApplication.kt
@@ -3,14 +3,12 @@ package be.casperverswijvelt.unifiedinternetqs
 import android.app.Application
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.content.Intent
 import android.util.Log
+import android.widget.Toast
 import be.casperverswijvelt.unifiedinternetqs.data.BITPreferences
 import be.casperverswijvelt.unifiedinternetqs.data.ShellMethod
-import be.casperverswijvelt.unifiedinternetqs.util.ExecutorServiceSingleton
-import be.casperverswijvelt.unifiedinternetqs.util.ShizukuUtil
-import be.casperverswijvelt.unifiedinternetqs.util.getInstallId
-import be.casperverswijvelt.unifiedinternetqs.util.initializeFirebase
-import be.casperverswijvelt.unifiedinternetqs.util.reportToAnalytics
+import be.casperverswijvelt.unifiedinternetqs.util.*
 import com.topjohnwu.superuser.Shell
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
@@ -18,8 +16,8 @@ import kotlinx.coroutines.runBlocking
 class TileApplication : Application() {
 
     companion object {
-        const val CHANNEL_ID = "tileSyncServiceChannel"
-        const val CHANNEL_NAME = "Tile Synchronization service"
+        const val CHANNEL_ID = "autoStartServiceChannel"
+        const val CHANNEL_NAME = "Shizuku Detection"
         const val TAG = "TileApplication"
     }
 
@@ -30,44 +28,50 @@ class TileApplication : Application() {
 
         ExecutorServiceSingleton.getInstance()
 
-        initializeFirebase(
-            this,
-            getInstallId(this)
-        )
-
         createNotificationChannel()
+        startTileSyncService()
 
         val preferences = BITPreferences(this)
         runBlocking {
-            when (preferences.getShellMethod.first()) {
-                ShellMethod.ROOT -> {
-                    Shell.getShell {
-                        reportToAnalytics(this@TileApplication)
-                    }
-                }
-
-                ShellMethod.SHIZUKU -> {
-                    reportToAnalytics(this@TileApplication)
-                }
-
-                ShellMethod.AUTO -> {
-                    // Mode AUTO is when user has not explicitly set a
-                    Shell.getShell {
-
-                        if (Shell.isAppGrantedRoot() == true) {
-                            runBlocking {
-                                preferences.setShellMethod(ShellMethod.ROOT)
-                            }
-                        } else if (ShizukuUtil.hasShizukuPermission()) {
-                            runBlocking {
-                                preferences.setShellMethod(ShellMethod.SHIZUKU)
-                            }
+            if (preferences.getShellMethod.first() == ShellMethod.AUTO) {
+                // Mode AUTO is when user has not explicitly set a
+                Shell.getShell {
+                    if (Shell.isAppGrantedRoot() == true) {
+                        runBlocking {
+                            preferences.setShellMethod(ShellMethod.ROOT)
                         }
-
-                        reportToAnalytics(this@TileApplication)
+                    } else if (ShizukuUtil.hasShizukuPermission()) {
+                        runBlocking {
+                            preferences.setShellMethod(ShellMethod.SHIZUKU)
+                        }
                     }
+
                 }
             }
+        }
+    }
+    private fun startTileSyncService() {
+        try {
+            Log.d(TAG, "Starting tile sync service!")
+            startForegroundService(
+                Intent(
+                    this,
+                    TileSyncService::class.java
+                )
+            )
+        } catch (e: Throwable) {
+            Log.d(
+                TAG,
+                "Failed to start tile sync foreground service due to an ${e.message}"
+            )
+
+            // Not sure what the cause of the 'ForegroundServiceStartNotAllowedException'
+            //  is or how to solve it.
+            Toast.makeText(
+                applicationContext,
+                R.string.toast_foreground_service_error,
+                Toast.LENGTH_SHORT
+            ).show()
         }
     }
 
